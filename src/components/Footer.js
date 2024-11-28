e206ad4038897f9aed80ca05d2e86759b5911d59
sanjita <sanjita.prem@gmail.com>
2024-11-28 06:50:20 +0530

    corrected url in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -37,16 +37,16 @@ const Footer = () => {
         <div className="footer-section">
           <h4>Follow Us</h4>
           <div className="social-icons">
-            <a href="#" aria-label="Facebook">
+            <a href="https://www.facebook.com/" aria-label="Facebook">
               <i className="fab fa-facebook-f"></i>
             </a>
-            <a href="#" aria-label="Twitter">
+            <a href="https://x.com/" aria-label="Twitter">
               <i className="fab fa-twitter"></i>
             </a>
-            <a href="#" aria-label="Instagram">
+            <a href="https://www.instagram.com/" aria-label="Instagram">
               <i className="fab fa-instagram"></i>
             </a>
-            <a href="#" aria-label="LinkedIn">
+            <a href="https://www.linkedin.com/" aria-label="LinkedIn">
               <i className="fab fa-linkedin-in"></i>
             </a>
           </div>
